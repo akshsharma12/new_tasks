142944532bf7048c08d659463c883ddda1b59901
akshsharma12 <harsh663535@gmail.com>
2024-02-22 16:07:30 +0530

    cors

--- swagger.js
+++ swagger.js
@@ -1,17 +1,14 @@
-const swaggerAutogen= require("swagger-autogen")
+const swaggerAutogen= require("swagger-autogen")();
 
 const option={
-     defination:{
-        openapi:"3.0.1",
-        info:{
+     info:{
             title:"crud_operation",
             version:"1.0.0",
             description:"A simple express crud_operation Api"
         },
-                host:"http://localhost:5000",
+                host:"localhost:5000",
                 basePath:"/"
      }
-}
 const outputFile="./swagger.json";
 const endpointsFiles=["./routes/route.js"];
 swaggerAutogen(outputFile,endpointsFiles,option)
